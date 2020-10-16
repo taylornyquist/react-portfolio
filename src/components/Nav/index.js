e3dd5aa9ab5d259586376bc69cde7246c383a3ae
Taylor Nyquist <tnyquist@gmail.com>
2020-10-16 13:15:43 -0500

    starting App and Nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+
+function Nav() {
+
+
+
+
+    return (
+
+        <h2>
+            Nav H2
+        </h2>
+
+
+
+    );
+
+
+
+
+
+
+}
+
+export default Nav;
