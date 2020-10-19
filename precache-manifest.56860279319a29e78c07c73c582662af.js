712f9d24444c282e8dca9561e3f269c5a1d44893
Taylor Nyquist <tnyquist@gmail.com>
2020-10-19 00:06:00 -0500

    Updates

---
+++ precache-manifest.56860279319a29e78c07c73c582662af.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c564152abec094df0c34614f10ed938b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d7ba55f663b4d9f87634",
+    "url": "/react-portfolio/static/css/main.e0ca94a5.chunk.css"
+  },
+  {
+    "revision": "2b4c1b1ddd816548a162",
+    "url": "/react-portfolio/static/js/2.c5f5d2bb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.c5f5d2bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ba55f663b4d9f87634",
+    "url": "/react-portfolio/static/js/main.363b7122.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "8fb4f2fb8e800974c126011fcd53fcd5",
+    "url": "/react-portfolio/static/media/Taylor Nyquist Resume.8fb4f2fb.pdf"
+  },
+  {
+    "revision": "62a0e9e5bfdc2e092e40c10a705a798e",
+    "url": "/react-portfolio/static/media/bio-pic.62a0e9e5.jpg"
+  },
+  {
+    "revision": "1b97584c7e47d1f6204ee1d886cbc7e3",
+    "url": "/react-portfolio/static/media/github-icon-12.1b97584c.svg"
+  },
+  {
+    "revision": "64bb200b1e4d9af2fb3d55bf1dd6be15",
+    "url": "/react-portfolio/static/media/github-icon.64bb200b.svg"
+  },
+  {
+    "revision": "ad912db0b8a4173590b8db9f6b7cc06d",
+    "url": "/react-portfolio/static/media/hero-bg.ad912db0.jpg"
+  },
+  {
+    "revision": "1186b158714e9a347c77d44d530193d5",
+    "url": "/react-portfolio/static/media/instagram-icon.1186b158.svg"
+  },
+  {
+    "revision": "3195844ee04ecd7917fd404eb68d66e6",
+    "url": "/react-portfolio/static/media/linkedin-icon.3195844e.svg"
+  },
+  {
+    "revision": "fb4bb91504de07d0ab8d07434e5e4a47",
+    "url": "/react-portfolio/static/media/portfolio-1.fb4bb915.jpg"
+  },
+  {
+    "revision": "cf6a561ff80e2041c4c13884f7a30ed9",
+    "url": "/react-portfolio/static/media/portfolio-2.cf6a561f.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/react-portfolio/static/media/portfolio-3.ed494730.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/react-portfolio/static/media/portfolio-4.12557c9b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/react-portfolio/static/media/portfolio-5.653c2b3d.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/react-portfolio/static/media/portfolio-6.938f8fe0.jpg"
+  }
+]);
